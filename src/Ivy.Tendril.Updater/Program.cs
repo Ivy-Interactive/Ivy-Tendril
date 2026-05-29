4622cdbc9d78855d15fb6eb5e8afd891985f59cb
Niels Bosma <niels@bosmainteractive.se>
2026-05-29 08:16:00 +0000

    fix: migrate Updater from Photino.NET to Ivy.Desktop (Rustino) (#976)
    Replaces the legacy Photino.NET windowing with Ivy.Desktop/Rustino,
    matching the approach used by the main Tendril app. Closes #961.

--- src/Ivy.Tendril.Updater/Program.cs
+++ src/Ivy.Tendril.Updater/Program.cs
@@ -1,7 +1,4 @@
-using System.Globalization;
-using System.Net.NetworkInformation;
-using System.Runtime.InteropServices;
-using Photino.NET;
+using Ivy.Desktop;
 
 namespace Ivy.Tendril.Updater;
 
@@ -10,18 +7,9 @@ internal static class Program
     [STAThread]
     public static int Main()
     {
-        return Updater().GetAwaiter().GetResult();
-    }
-
-    private static async Task<int> Updater()
-    {
-        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
-
-        var port = 5010;
-        var url = $"https://localhost:{port}";
         var server = new Server(new ServerArgs()
         {
-            Port = port,
+            Port = 5010,
             Silent = true,
             DefaultAppId = "loading"
         });
@@ -29,83 +17,19 @@ internal static class Program
         server.AddAppsFromAssembly();
         server.UseHotReload();
 
-        CancellationTokenSource cts = new CancellationTokenSource();
+        var iconResource = OperatingSystem.IsWindows() ? "Ivy.Tendril.Updater.icon.ico"
+            : OperatingSystem.IsMacOS() ? "Ivy.Tendril.Updater.icon.icns"
+            : "Ivy.Tendril.Updater.icon.png";
 
-        var serverTask = server.RunAsync(cts);
+        var window = new DesktopWindow(server)
+            .Title("Ivy Tendril Updater")
+            .Size(800, 600)
+            .Resizable(false)
+            .TopMost(true)
+            .UseDpiScaling(true)
+            .UseDevTools(false)
+            .Icon(typeof(Program), iconResource);
 
-        if (!await CheckIfPortIsListening(port))
-        {
-            Console.WriteLine($@"Error: Unable to connect to {url}. Something went wrong.");
-            return 1;
-        }
-
-        var window = new PhotinoWindow() { LogVerbosity = 0 };
-
-        var scalingFactor = DpiDetector.GetSystemScalingFactor();
-        var baseWidth = 800;
-        var baseHeight = 600;
-        var windowWidth = (int)(baseWidth * scalingFactor);
-        var windowHeight = (int)(baseHeight * scalingFactor);
-
-        var iconPath = GetIconPath();
-
-        window
-            .SetUseOsDefaultSize(false)
-            .SetSize(windowWidth, windowHeight)
-            .SetTitle("Ivy Tendril Updater")
-            .SetIconFile(iconPath)
-            .SetResizable(false)
-            .SetTopMost(true)
-            .SetJavascriptClipboardAccessEnabled(false)
-            .SetDevToolsEnabled(false)
-            .SetIgnoreCertificateErrorsEnabled(true)
-            .SetWebSecurityEnabled(false)
-            .Center()
-            .Load(new Uri(url));
-
-        window.WaitForClose();
-
-        await cts.CancelAsync();
-        await serverTask;
-
-        return 0;
-    }
-
-    private static string GetIconPath()
-    {
-        var baseDir = System.AppContext.BaseDirectory;
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            return Path.Combine(baseDir, "icon.ico");
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            return Path.Combine(baseDir, "icon.icns");
-        return Path.Combine(baseDir, "icon.png");
-    }
-
-    private static async Task<bool> CheckIfPortIsListening(int port, int maxAttempts = 10)
-    {
-        var delayMs = 1000;
-
-        for (var i = 0; i < maxAttempts; i++)
-        {
-            try
-            {
-                bool isListening = IPGlobalProperties
-                    .GetIPGlobalProperties()
-                    .GetActiveTcpListeners()
-                    .Any(endpoint => endpoint.Port == port);
-                if (isListening)
-                {
-                    return true;
-                }
-            }
-            catch
-            {
-                //ignore
-            }
-            if (i == maxAttempts - 1) return false;
-            await Task.Delay(delayMs);
-        }
-
-        return false;
+        return window.Run();
     }
 }
