99e8ba9b3f8238fcc40b83b8229e72efa8a98fea
rorychatt <pavel.rinne@gmail.com>
2026-07-16 12:18:59 +0200

    style: restore TagOpenConfig constant in SettingsApp

--- src/Ivy.Tendril/Apps/Settings/SettingsApp.cs
+++ src/Ivy.Tendril/Apps/Settings/SettingsApp.cs
@@ -20,6 +20,7 @@ public class SettingsApp : ViewBase
     internal const string TagProjects = "projects";
     private const string TagTunnel = "tunnel";
     private const string TagAdvanced = "advanced";
+    private const string TagOpenConfig = "open-config";
 
     public override object Build()
     {
